18a51c26c0d7c309b4d97cd61c250cb2104b398b
Gerald <geraldpng@gmail.com>
2020-11-26 20:16:53 +0800

    import basic.css in app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 // styles
 import { LocaleProvider } from '../components/locale/LocaleContext';
 import 'normalize.css';
+import '../styles/basic.css';
 
 // eslint-disable-next-line react/prop-types
 const App = ({ Component, pageProps }) => (
